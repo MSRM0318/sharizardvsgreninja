1e4a86c8482d290de0f6e16beb268297a5305738
MSRM0318 <125918238+MSRM0318@users.noreply.github.com>
2023-05-20 19:38:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -30,3 +30,24 @@ function resetGame() {
   document.getElementById('player-lives').textContent = playerLives;
   document.getElementById('enemy-lives').textContent = enemyLives;
 }
+
+var tit = document.title;
+var c = 0;
+function writetitle()
+{
+    document.title = tit.substring(0,c);
+    if(c==tit.length)
+{
+
+    c = 0;
+    setTimeout("writetitle()", 1000)
+}
+
+else
+{
+    c++;
+    setTimeout("writetitle()", 200)
+}
+}
+
+writetitle()
